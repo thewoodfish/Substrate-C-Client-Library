0279ea948ddcd4bb89ae00413f6421f3d44a8642
Woodfish <jasonholt2002@gmail.com>
2022-06-01 21:30:48 +0000

    latest block

--- test/test.c
+++ test/test.c
@@ -1,6 +1,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 struct Payload {
     double jsonrpc;
@@ -10,8 +11,17 @@ struct Payload {
     
 };
 
+// RPC message queue (a linked list)
+struct Req_queue {
+    char jsonrpc[5];
+    char* result;
+    int id;
+    struct Req_queue* next;
+} __RMQ; 
+
 char* json_dump_payload(struct Payload* p);
 char* slice(const char* str, char* result, size_t start, size_t end);
+extern struct Req_queue* parse_json_string(char* buf);
 
 char buffer[1024];
 char buf [1024];
@@ -26,17 +36,21 @@ char* slice(const char* str, char* result, size_t start, size_t end) {
 
 int main(void) {
 
-    struct Payload pl;
-    char* p[] = {
-        "Spain", "France", NULL
-    };
+    // struct Payload pl;
+    // char* p[] = {
+    //     "Spain", "France", NULL
+    // };
 
-    pl.jsonrpc = 2.0;
-    strcpy(pl.method, "get_storage");
-    pl.params = p;
-    pl.id = 100;
+    // pl.jsonrpc = 2.0;
+    // strcpy(pl.method, "get_storage");
+    // pl.params = p;
+    // pl.id = 100;
 
-    puts(json_dump_payload(&pl));
+    // puts(json_dump_payload(&pl));
+
+
+    char buf[] = "{\"jsonrpc\":\"2.0\",\"result\":\"4.0.0-dev-e9c1aac\",\"id\":\"1\"}";
+    parse_json_string(buf);
 
 }
 
@@ -57,4 +71,78 @@ char* json_dump_payload(struct Payload* p)
     sprintf(buffer, "{\"jsonrpc\":\"%f\",\"method\":\"%s\",\"params\":\"%s\",\"id\":\"%i\"}", p->jsonrpc, p->method, slice(buf, dummy, 0, count), p->id);
     
     return buffer;
+}
+
+struct Req_queue* parse_json_string(char* buf) {
+    // first allocate RMQ struct
+    struct Req_queue* rmq = malloc(sizeof(__RMQ));
+    
+    char* str = buf;
+    char* s1 = rmq->jsonrpc;
+
+    char space[1024];
+    char* s2 = space;
+
+    char box[10];
+    char* s3 = box;
+
+    // some sort of stack base pointer
+    char* usp;
+
+    int i, count;
+
+    // extract result, id and version
+    i = count = 0;
+
+    while (*str) {
+        if (*str == ':') {
+            // parse "jsonrpc"
+            if (!i) {
+                str += 2; // skip ':"'
+                while (*str != '"') {
+                    *s1 = *str;
+                    str++; s1++;
+                }
+
+            } else if (i == 1) {
+                // parse "result"
+
+                // keep base pointer
+                usp = str;
+
+                str += 2; // skip ':"'
+                while (*str != '"') {
+                    count++;
+                    str++;
+                }
+
+                // return back to usp
+                str = usp;
+
+                str += 2;
+                while (*str != '"') {
+                    *s2 = *str;
+                    str++; s2++;
+                }
+
+            } else {
+                str += 2; // skip ':"'
+                while (*str != '"') {
+                    *s3 = *str;
+                    str++; s3++;
+                }
+            }
+
+            i++;
+        }
+        str++;
+    }
+
+    // allocate space for result
+    rmq->result = (char*) malloc(count + 1);
+    strcpy(rmq->result, space);
+
+    rmq->id = atoi(box);
+
+    return rmq;
 }
